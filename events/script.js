9f497d1af02dd577ad9d1859b1d63c2217f28d3c
aliawaleed <57350259+aliawaleed@users.noreply.github.com>
2022-02-09 23:40:10 +0400

    updated code

--- events/script.js
+++ events/script.js
@@ -1,63 +1,69 @@
 //arrays for all images
-var topList = ["tops1", "tops2", "tops3", "tops4", "tops5"]; 
+var topList = ["tops1", "tops2", "tops3", "tops4", "tops5", "tops6"]; 
 var bottomList = ["bottom1", "bottom2", "bottom3", "bottom4", "bottom5"];
 var shoeList = ["shoes1", "shoes2", "shoes3", "shoes4", "shoes5"];
 var toyList = ["toy1", "toy2", "toy3", "toy4", "toy5"];
 
-var counter = 0;
-var cnt = 0;
+//check which element of array it's on
+var topcnt = 0;
+var bottomcnt = 0;
 var shoecnt = 0;
+var toycnt = 0;
 
+//audio variable
+var x = document.getElementById("twinkle"); 
+
+// declaring variables to increase and decrease for tops, bottoms, and shoes
+let inc = document.getElementById('top_increase');
+let dec = document.getElementById('top_decrease');
+let incb = document.getElementById('bottom_increase');
+let decb = document.getElementById('bottom_decrease');
+let incs = document.getElementById('shoe_increase');
+let decs = document.getElementById('shoe_decrease');
+let shuffle = document.getElementById('shuffle');
+
+
+////////////////////////////////*     Tops      *////////////////////////////////
 //next one image: tops
 function t_increase() {
-    document.getElementById("topPhoto").src = "images/" + topList[counter] + ".jpg";
-    if (counter >= topList.length - 1) {
-        counter = 0;
+    document.getElementById("topPhoto").src = "images/" + topList[topcnt] + ".jpg";
+    if (topcnt >= topList.length - 1) {
+        topcnt = 0;
     } else {
-        counter += 1;
+        topcnt += 1;
     }
 }
-
 //back one image: tops
 function t_decrease() {
-    document.getElementById("topPhoto").src = "images/" + topList[counter] + ".jpg";
-    if (counter <= 0) {
-        counter = topList.length - 1;
+    document.getElementById("topPhoto").src = "images/" + topList[topcnt] + ".jpg";
+    if (topcnt <= 0) {
+        topcnt = topList.length - 1;
     } else {
-        counter -= 1;
+        topcnt -= 1;
     }
 }
 
-let inc = document.getElementById('top_increase');
-inc.addEventListener('click', t_increase);  
-let dec = document.getElementById('top_decrease');
-dec.addEventListener('click', t_decrease);  
-
+////////////////////////////////*     Bottoms      *////////////////////////////////
 //next one image: bottoms
 function b_increase() {
-    document.getElementById("bottomPhoto").src ="images/" + bottomList[cnt] + ".jpg";
-    if (cnt >= bottomList.length - 1) {
-        cnt = 0;
+    document.getElementById("bottomPhoto").src ="images/" + bottomList[bottomcnt] + ".jpg";
+    if (bottomcnt >= bottomList.length - 1) {
+        bottomcnt = 0;
     } else {
-        cnt += 1;
+        bottomcnt += 1;
     }
 }
-
 //back one image: bottoms
 function b_decrease() {
-    document.getElementById("bottomPhoto").src ="images/" + bottomList[cnt] + ".jpg";
-    if (cnt <= 0) {
-        cnt = bottomList.length - 1;
+    document.getElementById("bottomPhoto").src ="images/" + bottomList[bottomcnt] + ".jpg";
+    if (bottomcnt <= 0) {
+        bottomcnt = bottomList.length - 1;
     } else {
-        cnt -= 1;
+        bottomcnt -= 1;
     }
 }
   
-let incb = document.getElementById('bottom_increase');
-incb.addEventListener('click', b_increase);  
-let decb = document.getElementById('bottom_decrease');
-decb.addEventListener('click', b_decrease);  
-
+////////////////////////////////*     Shoes      *////////////////////////////////
 //next one image: shoes
 function s_increase() {
     document.getElementById("shoePhoto").src ="images/" + shoeList[shoecnt] + ".jpg";
@@ -67,7 +73,6 @@ function s_increase() {
     shoecnt += 1;
     }
 }
-
 //back one image: shoes
 function s_decrease() {
     document.getElementById("shoePhoto").src ="images/" + shoeList[shoecnt] + ".jpg";
@@ -78,33 +83,45 @@ function s_decrease() {
     }
 }
     
-let incs = document.getElementById('shoe_increase');
-incs.addEventListener('click', s_increase);  
-let decs = document.getElementById('shoe_decrease');
-decs.addEventListener('click', s_decrease); 
-
+////////////////////////////////*     Toys      *////////////////////////////////
 //keyboard control for toys - 37 for right arrow: next / 39 for left arrow: back 
 document.addEventListener('keydown', (e) => {
     e = e || window.event;
     if (e.keyCode === 37) {
-      document.getElementById("toyPhoto").src = "images/" + toyList[cnt] + ".jpg";
-        if (cnt <= 0) {
-            cnt = toyList.length - 1;
+      document.getElementById("toyPhoto").src = "images/" + toyList[toycnt] + ".jpg";
+        if (toycnt <= 0) {
+            toycnt = toyList.length - 1;
         } else {
-            cnt -=1;
+            toycnt -=1;
         }
     } else if (e.keyCode === 39) {
-      document.getElementById("toyPhoto").src = "images/" + toyList[cnt] + ".jpg";
-        if (cnt >= toyList.length - 1) {
-            cnt = 0;
+      document.getElementById("toyPhoto").src = "images/" + toyList[toycnt] + ".jpg";
+        if (toycnt >= toyList.length - 1) {
+            toycnt = 0;
         } else {
-            cnt += 1;
+            toycnt += 1;
         }
     }
   })
 
+// audio control
+function playAudio() { 
+    x.play(); 
+  } 
+  
+  function pauseAudio() { 
+    x.pause(); 
+  } 
+
+// associating increase and decrease with click
+inc.addEventListener('click', t_increase);  
+dec.addEventListener('click', t_decrease);  
+incb.addEventListener('click', b_increase);  
+decb.addEventListener('click', b_decrease);  
+incs.addEventListener('click', s_increase);  
+decs.addEventListener('click', s_decrease); 
+
 //change all outfit together with next image for each
-let shuffle = document.getElementById('shuffle');
 shuffle.addEventListener('click', t_increase);
 shuffle.addEventListener('click', b_increase);  
 shuffle.addEventListener('click', s_increase);
